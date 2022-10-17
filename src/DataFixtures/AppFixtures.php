2304c56ab2a556ebe6ff177bfb25727d788fb201
Nesrine ANAFAL <nanafal2021@campus-eni.fr>
2022-10-17 16:32:23 +0200

    Ajout facker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,51 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Participant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $userPasswordHasher;
+    private Generator $generator;
+    private ObjectManager $manager;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+        $this->generator = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $this->manager = $manager;
+
+        $this->addUsers();
         // $product = new Product();
         // $manager->persist($product);
 
-        $manager->flush();
+        //$manager->flush();
+    }
+
+    public function addUsers()
+    {
+        for($i = 0; $i < 3; $i++){
+
+            $user = new Participant();
+            $user->setRoles(['ROLE_USER'])
+                ->setEmail($this->generator->email)
+                ->setFirstname($this->generator->firstName)
+                ->setLastname($this->generator->lastName)
+                ->setPassword($this->userPasswordHasher->hashPassword($user, 'toto'))
+                ->setActive(1)
+                ->setPhone($this->generator->phoneNumber);
+
+            $this->manager->persist($user);
+        }
+        $this->manager->flush();
     }
 }

7991e73142130191353004eb59a842a0846dbb50
Kylian V <50665740+DevKyky@users.noreply.github.com>
2022-10-17 13:40:11 +0200

    Minicommit Profile

---
+++ src/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'required' => true
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true
+            ])
+            ->add('telephone', TextType::class, [
+                'required' => true
+            ])
+            ->add('mail', EmailType::class, [
+                'required' => true
+            ])
+            ->add('administrateur', ChoiceType::class, [
+                'expanded' => true,
+                'multiple' => true,
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}

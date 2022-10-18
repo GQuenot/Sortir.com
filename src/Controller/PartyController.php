49563ac3d8f76f69b4f27e8025c7663de4064a4d
Gurvan <gurvan.quenot@pm.me>
2022-10-18 16:30:54 +0200

    Refactorisation

--- src/Controller/PartyController.php
+++ src/Controller/PartyController.php
@@ -7,6 +7,7 @@ use App\Form\PartyType;
 use App\Repository\SortieRepository;
 use App\Services\PartyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,26 +25,31 @@ class PartyController extends AbstractController
     public function add(Request $request): Response
     {
         $party = new Sortie();
-        $partyForm = $this->createForm(PartyType::class, $party);
-
-        $partyForm->handleRequest($request);
-
-        if ($partyForm->isSubmitted() && $partyForm->isValid()) {
-            $this->partyService->saveParty($party);
-            $this->addFlash('success', 'La sortie a bien été enregistrée.');
-
-            return $this->redirectToRoute('party_add');
-        }
-
-        return $this->render('party/add.html.twig', [
-            'partyForm' => $partyForm->createView()
-        ]);
+        return $this->saveParty($party, $request);
     }
 
     #[Route('/edit/{id}', name: 'edit')]
     public function edit(int $id, Request $request, SortieRepository $sortieRepository): Response
     {
         $party = $sortieRepository->find($id);
+
+        if($party->getState()->getLabel() != 'Créée') {
+            $this->addFlash('warning', 'Une demande déja publiée ne peut être modifiée');
+            return $this->redirectToRoute('party_add');
+
+            //throw new Exception('Une demande déjà publiée ne peut être modifiée');
+        }
+
+        return $this->saveParty($party, $request);
+    }
+
+    /**
+     * @param Sortie $party
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function saveParty(Sortie $party, Request $request): Response|RedirectResponse
+    {
         $partyForm = $this->createForm(PartyType::class, $party);
 
         $partyForm->handleRequest($request);

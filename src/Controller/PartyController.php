8c7d1dee0adee1503d206284e7f55a2f792f9e78
Kylian V <50665740+DevKyky@users.noreply.github.com>
2022-10-18 12:09:42 +0200

    Add Sortie

---
+++ src/Controller/PartyController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\PartyType;
+use App\Repository\EtatRepository;
+use App\Repository\ParticipantRepository;
+use App\Repository\SiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/party', name: 'party_')]
+class PartyController extends AbstractController
+{
+    #[Route('/add', name: 'add')]
+    public function add(Request $request, EntityManagerInterface $entityManager, SiteRepository $siteRepository, ParticipantRepository $participantRepository, EtatRepository $etatRepository): Response
+    {
+        $party = new Sortie();
+        $partyForm = $this->createForm(PartyType::class, $party);
+
+        $partyForm->handleRequest($request);
+
+        if ($partyForm->isSubmitted() && $partyForm->isValid()) {
+
+            $organizer = $participantRepository->findOneBy(['email' => 'kvincent@orange.fr']); //$this->getUser()->getUserIdentifier()
+            $site = $siteRepository->findOneBy(['id' => $organizer]);
+            $state = $etatRepository->findOneBy(['label' => 'Créée']);
+
+            $party->setOrganizer($organizer)
+                ->setState($state)
+                ->setSite($site);
+
+            $entityManager->persist($party);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La sortie a bien été enregistrée.');
+
+            return $this->redirectToRoute('party_add');
+        }
+
+        return $this->render('party/add.html.twig', [
+            'partyForm' => $partyForm->createView()
+        ]);
+    }
+}
---
+++ src/Form/PartyType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class PartyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true
+            ])
+            ->add('startDate', DateTimeType::class, [
+                'required' => true,
+                'widget' => 'single_text',
+                'html5' => true,
+                'constraints' => [
+                    new GreaterThan([
+                        'value' => new \DateTime()
+                    ])
+                ]
+            ])
+            ->add('duration', NumberType::class, [
+                'required' => true,
+                'constraints' => [
+                    new GreaterThanOrEqual([
+                        'value' => 60
+                    ])
+                ]
+            ])
+            ->add('subLimitDate', DateTimeType::class, [
+                'required' => true,
+                'html5' => true,
+                'widget' => 'single_text',
+                'constraints' => [
+                    new GreaterThan([
+                        'value' => new \DateTime()
+                    ])
+                ]
+            ])
+            ->add('maxSubscription', NumberType::class, [
+                'required' => true,
+                'constraints' => [
+                    new GreaterThanOrEqual([
+                        'value' => 1
+                    ])
+                ]
+            ])
+            ->add('informations', TextareaType::class)
+            ->add('place', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}

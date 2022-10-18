6f660a5fa48361114d7335149da24ba6f83cc1f6
lenaig-achrd <lena.lecocq@hotmail.fr>
2022-10-18 09:25:19 +0200

    login

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Participant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,23 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        $participant = Array();
+        for ($i = 0; $i < 4; $i++) {
+            $participant[$i] = new Participant();
+            $participant[$i]->setEmail($faker->email);
+            $participant[$i]->setFirstname($faker->firstName);
+            $participant[$i]->setLastname($faker->lastname);
+            $participant[$i]->setPassword('123456');
+            $participant[$i]->setPhone($faker->phoneNumber);
+            $participant[$i]->setActive(1);
+            $participant[$i]->setRoles(["ROLE_USER"]);
+            $participant[$i]->setPseudo($faker->userName);
+
+            $manager->persist($participant[$i]);
+        }
 
         $manager->flush();
     }
